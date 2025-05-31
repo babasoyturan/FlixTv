6df744c4a0501adf917e79a0ae99967cd9b62310
Turan Babasoy <babasoyturan1@gmail.com>
2025-05-31 13:57:29 +0400

    Add project files.

---
+++ src/Clients/MvcWeb/FlixTv.Clients.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using FlixTv.Clients.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FlixTv.Clients.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Clients/MvcWeb/FlixTv.Clients.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlixTv.Clients.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
